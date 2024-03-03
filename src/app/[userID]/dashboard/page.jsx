21736bd2390f8f22c6a2dd844c62bcba0a8cdf1d
Abhishek Anand <112304737+abhishekanandok@users.noreply.github.com>
2024-03-03 11:58:56 +0530

    dynamic params

--- src/app/[userID]/dashboard/page.jsx
+++ src/app/[userID]/dashboard/page.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
-
-const dashboard = () => {
-  return (
-    <div>dashboard</div>
-  )
+export default function Page({ params }) {
+  return <div className=" text-center">
+    <h1>Dashboard</h1>
+    <h2>Username: {params.userID}</h2>
+  </div>
 }
-
-export default dashboard
--- src/app/[userID]/profile/page.jsx
+++ src/app/[userID]/profile/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const profile = () => {
+const profile = ({ params }) => {
   return (
-    <div>profile</div>
+    <div className=' text-center'>
+      <h1>Profile</h1>
+      <h2>Username :- {params.userID}</h2>
+    </div>
   )
 }
 
